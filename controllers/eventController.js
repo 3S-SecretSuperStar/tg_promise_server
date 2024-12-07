e346fbd42b4f3e2af215a1a43989f4257dc1edb4
3S-SecretSuperStar <juliosantos95617@gmail.com>
2024-12-06 18:43:27 -0700

    feat test send data

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -44,7 +44,7 @@ async function fetchUpcomingEvents(leagueId) {
 
   // Sort by date ascending
   upcomingEvents.sort((a, b) => new Date(a.dateEvent) - new Date(b.dateEvent));
-  pastEvents.sort((a, b) => new Date(b.dateEvent) - new Date(a.dateEvent));
+  pastEvents.sort((a, b) => new Date(a.dateEvent) - new Date(b.dateEvent));
 
   return { upcomingEvents: upcomingEvents, pastEvents: pastEvents.slice(0,5) };
 }
