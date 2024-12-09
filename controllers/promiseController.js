50e45d79ce91e229d93e279e97fb1b6a0e1a3920
3S-SecretSuperStar <juliosantos95617@gmail.com>
2024-12-09 12:08:26 -0700

    feat add console

--- controllers/promiseController.js
+++ controllers/promiseController.js
@@ -3,6 +3,7 @@ const { Promise } = require('../models/model');
 // Create a Promise
 const createPromise = async (req, res) => {
   const { creatorId, description, betAmount, creatorChoice, resolutionDate } = req.body;
+  console.log("promise : ",res.body)
   const newPromise = new Promise({
     creator_id: creatorId,
     description: description,
