b928f26955877e05eea8969e0458091df79f57d7
3S-SecretSuperStar <juliosantos950527@gmail.com>
2024-12-13 18:30:24 -0700

    feat update create promise

--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,8 @@
 const { User } = require("../models/model")
-
-
-
-export const checkUserBalance = async(userObjectId,amount) =>{
+const checkUserBalance = async(userObjectId,amount) =>{
   const targetObjectId = new mongoose.Types.ObjectId(userObjectId)
   const userInfo = await User.findById(targetObjectId);
   return userInfo.amount>=amount;
 }
+
+module.exports({checkUserBalance})
