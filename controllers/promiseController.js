9c78338e86cfd4b3f6a5c8e3baa61ea085384696
3S-SecretSuperStar <juliosantos950527@gmail.com>
2024-12-13 21:50:26 -0700

    feat update create promise

--- controllers/promiseController.js
+++ controllers/promiseController.js
@@ -1,4 +1,4 @@
-const { default: mongoose } = require('mongoose');
+const mongoose = require('mongoose')
 const { Promise, User } = require('../models/model');
 const { checkUserBalance } = require('../utils');
 
