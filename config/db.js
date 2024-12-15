2d022516ad3e5716fbe927f0d5da06998205ba8e
3S-SecretSuperStar <juliosantos950527@gmail.com>
2024-12-15 01:36:06 -0700

    add cron job

--- config/db.js
+++ config/db.js
@@ -5,10 +5,7 @@ dotenv.config();
 
 const connectDB = async() =>{
   try{
-    await mongoose.connect(process.env.MONGO_URL,{
-      useNewUrlParser:true,
-      // userUndefiedTopology:true
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('MongoDB connected!')
   }catch(error){
     console.log('MongoDB connection failed',error.message)
